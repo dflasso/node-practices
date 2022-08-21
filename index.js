71b9a7f7a531ffdb8f4f451e377653a484d0c8f8
dlassoa <dlasso@ec.krugercorp.com>
2022-08-20 20:44:29 -0500

    additional infotmation about node.js with express.js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
+//middleware
+app.use(express.json())
+
+//http://localhost:3000/variable?limit=5&offset=10
+app.get("/:pathVariable", (req, res ) => {
+    const {pathVariable} = req.params;
+    const {limit , offset} = req.query;
+    console.log(`pathVariable : ${pathVariable}    query params : ${limit}, ${offset}`)
+    res.send({
+        test: "ss"
+    })
+})
+
+app.listen(port , () => {
+    console.log('Started App in localhost:3000')
+})
